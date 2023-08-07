d92204809d5a501ffccb8b304781b99653af233c
Gallardo994 <evolutionv8@yandex.ru>
2023-08-07 07:00:40 +0400

    Any process

--- Program.cs
+++ Program.cs
@@ -1,11 +1,32 @@
 ﻿using System.Diagnostics;
 
+var processListFile = "processlist.txt";
+if (!File.Exists(processListFile))
+{
+    Console.WriteLine("processlist.txt not found, press any key to exit");
+    Console.ReadKey();
+}
+
+var processList = File.ReadAllLines(processListFile);
+
+foreach (var processName in processList)
+{
+    Console.WriteLine($"Process: {processName}");
+}
+
+var processListHash = new HashSet<string>(processList);
+
 while (true)
 {
-    var processes = Process.GetProcessesByName("Blacklist_DX11_game");
+    var processes = Process.GetProcesses();
     
     foreach (var process in processes)
     {
+        if (!processListHash.Contains(process.ProcessName))
+        {
+            continue;
+        }
+        
         try
         {
             process.ProcessorAffinity = (IntPtr) 0xFFFFFFFF;
